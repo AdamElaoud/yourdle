14e4baab937fb51ff393e48608abdc0e29f6a2cc
Adam Elaoud <adam.elaoud@gmail.com>
2022-03-01 20:58:38 -0700

    complete basic implementation of guess validator

--- src/utility/validator.js
+++ src/utility/validator.js
@@ -1,19 +1,31 @@
-//enum
+// enum
 const LetterState = {
-    miss: 0,
-    present: 1,
-    match: 2
+    MISS: "MISS",
+    PRESENT: "PRESENT",
+    MATCH: "MATCH"
 };
 
 
 /*
     params
-    - guess       [] | array representing the guesses for each letter in the word
-    - answer      [] | array representing the correct word to be guessed
+    - guess      str | word guess
+    - answer     str | correct word to be guessed
 
     returns
-    - result      [] | array of LetterState values
+    - result      [] | array of LetterState values corresponding to each letter in the correct word
 */
-function validate() {
+function validate(guess, answer) {
+    const guessLetters = guess.split("");
+    const answerLetters = answer.split("");
 
+    const result = [];
+
+    guessLetters.forEach((letter, index) => {
+        if (letter != answerLetters[index] && answerLetters.includes(letter))
+            result.push(LetterState.PRESENT);
+        else if (letter == answerLetters[index])
+            result.push(LetterState.MATCH);
+        else
+            result.push(LetterState.MISS);
+    });
 }
